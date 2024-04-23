d02ba2d5e78f0e9178ab85aadfd070025145fdee
Christ KATUMBA <christkatumba02@gmail.com>
2024-04-23 17:34:02 +0100

    Modification du fonction longtime_add

--- test_celery/tasks.py
+++ test_celery/tasks.py
@@ -1,15 +1,9 @@
 from __future__ import absolute_import
 from test_celery.celery import app
-import logging
 import time
 
-# Configuration du logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO) 
 
 @app.task
 def longtime_add(x, y):
-    logger.info(f"Tâche add({x}, {y}) démarrée")
-    result = x + y
-    logger.info(f"Tâche add({x}, {y}) terminée avec succès. Résultat : {result}")
-    return result
+    time.sleep(5)
+    return x + y
